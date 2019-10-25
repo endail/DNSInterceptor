73b53a99aece1a71773885e87ab54117caa89790
endail <52652357+endail@users.noreply.github.com>
2019-10-25 20:02:14 +1100

    remove quotes

--- dnsinterceptor/run.py
+++ dnsinterceptor/run.py
@@ -1,7 +1,7 @@
 from PythonDNS.InterceptResolver import InterceptResolver
 from dnslib.server import DNSLogger, DNSServer
 import argparse, time
-from .helpers import loadConfig
+from helpers import loadConfig
 
 p = argparse.ArgumentParser(prog="DNSInterceptor")
 
@@ -14,23 +14,24 @@ args = p.parse_args()
 config = loadConfig(args.config_file)
 
 print("Starting DNSInterceptor on %s:%d, forwarding to %s:%d" % (
-    config['local']['address'], config['local']['port'],
-    config['upstream']['address'], config['upstream']['port']
+    config['local']['address'], int(config['local']['port']),
+    config['upstream']['address'], int(config['upstream']['port'])
 ))
 
 resolver = InterceptResolver(
     config['upstream']['address'],
-    config['upstream']['port'],
+    int(config['upstream']['port']),
     config['filters']['cloakfile'],
     config['filters']['blacklistfile'],
-    config['upstream']['timeout'])
+    int(config['upstream']['timeout'])
+)
 
 print("<%d CloakRules Imported>" % len(resolver.cloakrules))
 print("<%d BlacklistRules Imported>" % len(resolver.blacklistrules))
 
 udp_server = DNSServer(
     resolver,
-    port=config['local']['port'],
+    port=int(config['local']['port']),
     address=config['local']['address'])
 
 udp_server.start_thread()
