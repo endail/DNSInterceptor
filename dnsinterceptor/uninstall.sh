b9994cefcafe69ff0e33b60ccf549ddd210b03df
endail <52652357+endail@users.noreply.github.com>
2019-10-30 19:39:39 +1100

    root uninstall

--- dnsinterceptor/uninstall.sh
+++ dnsinterceptor/uninstall.sh
@@ -1,4 +1,10 @@
 
+# Check if root user
+if [ `id -u` -ne 0 ]; then
+ echo "Uninstall as root user"
+ exit 1;
+fi
+
 # Stop/disable the service
 systemctl stop dnsinterceptor
 systemctl disable dnsinterceptor
